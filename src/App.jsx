fb10c6c9738402a578bce12cb02027ec702cb2f1
Branham Karanja <branham.karanja@outlook.com>
2025-03-19 18:29:20 +0300

    chore: install and set up React Router with initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { Landing } from "./pages";
+
 function App() {
-	
-
-	
-
-	return <><h1 className="text-amber-600">Hello, world!</h1></>;
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Landing />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
