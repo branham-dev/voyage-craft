42079e912db1a7a2936a9267d3881921104bbe5f
Branham Karanja <branham.karanja@outlook.com>
2025-03-19 15:45:24 +0300

    refactor: move API fetch logic to custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import axios from "axios";
-import { useEffect } from "react";
-
 function App() {
-	const fetchData = async (searchQuery) => {
-		try {
-			const response = await axios.get(`http://localhost:5000/api/tripadvisor/location/search`, {
-				params: { searchQuery, category: "hotels" },
-			});
-			const data = await response.data;
-			console.log(data);
-		} catch (error) {
-			console.error("Error fetching locations:", error.response?.data || error.message);
-			return null;
-		}
-	};
+	
 
-	useEffect(() => {
-		(async () => {
-			await fetchData("Nairobi");
-		})();
-	}, []);
+	
 
 	return <></>;
 }
