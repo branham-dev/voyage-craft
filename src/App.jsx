1ca5cf0f211e9e826099d98cbf66644275676ebb
Branham Karanja <branham.karanja@outlook.com>
2025-03-19 15:29:02 +0300

    feat: integrate initial API call in App.jsx for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function App() {
+	const fetchData = async (searchQuery) => {
+		try {
+			const response = await axios.get(`http://localhost:5000/api/tripadvisor/location/search`, {
+				params: { searchQuery, category: "hotels" },
+			});
+			const data = await response.data;
+			console.log(data);
+		} catch (error) {
+			console.error("Error fetching locations:", error.response?.data || error.message);
+			return null;
+		}
+	};
+
+	useEffect(() => {
+		(async () => {
+			await fetchData("Nairobi");
+		})();
+	}, []);
+
 	return <></>;
 }
 
